7de3e53980065d02e755404c9f1051e4a056442a
Pirvu Daniel Catalin <daniel.catalin.pirvu@gmail.com>
2021-07-30 22:11:47 +0300

    Update

---
+++ TrustNews_SingleNode.sh
@@ -0,0 +1,45 @@
+#############################################################################################
+# Summary #
+#############################################################################################
+# Node      || BigchainDB API   || BigchainDB Websocket || MongoDB  || Tendermint RPC
+# Node 1    || 9981             || 9982                 || 27011    || 26651
+#############################################################################################
+action=$1
+script_path="D:/Facultate/VMs/.SharedFolder/TrustNews"
+# script_path=$(dirname $(realpath $0))
+
+case "$action" in
+    "create")
+        echo "Creating node 1"
+        docker run \
+        --detach \
+        --name bigchaindbnode1 \
+        --publish 9981:9984 \
+        --publish 9982:9985 \
+        --publish 27011:27017 \
+        --publish 26651:26657 \
+        --volume ${script_path}/Node1/MongoDB/data/db:/data/db \
+        --volume ${script_path}/Node1/MongoDB/data/configdb:/data/configdb \
+        --volume ${script_path}/Node1/Tendermint/tendermint:/tendermint \
+        bigchaindb/bigchaindb:all-in-one
+        echo "Node 1 created"
+        ;;
+    "start")
+        echo "Starting node 1"
+        docker start bigchaindbnode1
+        echo "Node 1 started"
+        ;;
+    "stop")
+        echo "Stopping node 1"
+        docker stop bigchaindbnode1
+        echo "Node 1 stopped"
+        ;;
+    "clean")
+        bash ./TrustNews.sh stop
+        rm -rf ${script_path}/Node1/MongoDB
+        rm -rf ${script_path}/Node1/Tendermint
+        docker rm -v bigchaindbnode1
+        ;;
+    *)
+        echo "Action is not registered"
+esac
